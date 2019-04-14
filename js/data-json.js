4a4de1a681968e1f05b77467e5ddebee8d86949c
tgwisdom <47089305+tgwisdom@users.noreply.github.com>
2019-04-14 16:19:04 -0500

    Add files via upload

--- js/data-json.js
+++ js/data-json.js
@@ -6,7 +6,7 @@ var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
 
 xhr.onload = function() {                       // When readystate changes
   // The following conditional check will not work locally - only on a server
- // if(xhr.status === 200) {                      // If server status was ok
+  if(xhr.status === 200) {                      // If server status was ok
     responseObject = JSON.parse(xhr.responseText);
 
     // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
@@ -23,7 +23,7 @@ xhr.onload = function() {                       // When readystate changes
     // Update the page with the new content
     document.getElementById('content').innerHTML = newContent;
 
- // }
+  }
 };
 
 xhr.open('GET', 'data/data.json', true);        // Prepare the request
